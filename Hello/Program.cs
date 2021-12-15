101134f673117a7e9dfde49ba7d5fed968096bf5
Anto Subash <antosubash@live.com>
2021-12-15 20:57:55 +0100

    hello world.

---
+++ Hello/Program.cs
@@ -0,0 +1,41 @@
+﻿using Volo.Abp;
+using Microsoft.AspNetCore.Mvc;
+using Volo.Abp.AspNetCore.Mvc;
+using Volo.Abp.Autofac;
+using Volo.Abp.Modularity;
+using Volo.Abp.DependencyInjection;
+
+var builder = WebApplication.CreateBuilder(args);
+builder.Host.AddAppSettingsSecretsJson()
+    .UseAutofac();
+builder.Services.ReplaceConfiguration(builder.Configuration);
+builder.Services.AddApplication<MinimalModule>();
+var app = builder.Build();
+
+app.MapGet("/hi", ([FromServices] HelloService repository) =>
+{
+    return repository.SayHi();
+});
+
+app.InitializeApplication();
+app.Run();
+
+public class HelloService : ITransientDependency
+{
+    public string SayHi()
+    {
+        return "Hi from service";
+    }
+}
+
+[DependsOn(
+    typeof(AbpAspNetCoreMvcModule),
+    typeof(AbpAutofacModule)
+)]
+public class MinimalModule : AbpModule
+{
+    public override void ConfigureServices(ServiceConfigurationContext context)
+    {
+        context.Services.AddAssemblyOf<MinimalModule>();
+    }
+}
